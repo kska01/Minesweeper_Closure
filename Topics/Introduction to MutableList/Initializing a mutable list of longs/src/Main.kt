7ff408b5281ebb913e798a04a930c8d411d13783
ka <insideka01@gmail.com>
2022-07-16 11:27:04 +0900

    07.16

---
+++ Topics/Introduction to MutableList/Initializing a mutable list of longs/src/Main.kt
@@ -0,0 +1,10 @@
+const val ELEMENT1 = 100_000_000_001
+const val ELEMENT2 = 100_000_000_002
+const val ELEMENT3 = 100_000_000_003
+
+fun main() {
+    val longs = mutableListOf(ELEMENT1, ELEMENT2, ELEMENT3)
+
+    // do not touch the lines below 
+    println(longs.joinToString())
+}
